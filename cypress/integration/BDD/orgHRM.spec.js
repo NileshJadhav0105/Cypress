f943988c0c8170eb5da3f9f4e02cd7a6fe5a55a3
nileshjadhav0105 <nileshjadhav0105nj@gmail.com>
2022-06-22 20:56:26 +0530

    BDD org

--- cypress/integration/BDD/orgHRM.spec.js
+++ cypress/integration/BDD/orgHRM.spec.js
@@ -1,44 +1,22 @@
-// import { Given, When, And, Then } from "cypress-cucumber-preprocessor/steps";
-
-// Given("visit org HRM site", () => {
-//   cy.log("Hello");
-// });
-
-// When("valid username", () => {
-//   cy.log("Hi...");
-// });
-
-// When("valid passeord", () => {
-//   cy.log("Hey!");
-// });
-
-// And("click on Login button", () => {
-//   cy.log("Bye!");
-// });
-
-// Then("Validate Dashboad", () => {
-//   cy.log("Good Day");
-// });
-
-
-import { Given, When, And, Then } from "cypress-cucumber-preprocessor"
+import { Given, When, And, Then } from "cypress-cucumber-preprocessor/steps";
 
 Given("visit org HRM site", () => {
-    cy.visit('https://opensource-demo.orangehrmlive.com/index.php/dashboard')
-})
+  cy.log("Hello");
+});
 
-And('valid username', () => {
-    cy.get('#txtUsername').type('Admin')
-})
+When("valid username", () => {
+  cy.log("Hi...");
+});
 
-And('valid passeord', () => {
-    cy.get('#txtPassword').type('admin123')
-})
+When("valid passeord", () => {
+  cy.log("Hey!");
+});
 
-When('click on Login button', () => {
-    cy.get('#btnLogin').click()
-})
+And("click on Login button", () => {
+  cy.log("Bye!");
+});
 
-Then('Validate Dashboad', () => {
-    cy.contains('Dashboard').should('be.visible')
-})
+Then("Validate Dashboad", () => {
+  cy.log("Good Day");
+});
+
